d681a3c4660b9a55464e468c68506986237ccfbe
klimenkoAnna <milaanya@yandex.ru>
2020-11-09 03:17:48 +0300

    add tests

---
+++ Examples/Testing/UnitTestsKlimenko/DayShiftServiceTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+using UnitTestsTarget;
+
+namespace UnitTestsKlimenko
+{
+    public class DayShiftServiceTest
+    {
+        private DayShiftService _dayShiftService;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _dayShiftService = new DayShiftService(new DayOfWeekService());
+        }
+
+        private static object[] _dateCasesBusiness =
+        {
+            new object[] {TestUtils.GetDate("06/11/2020"), 1, TestUtils.GetDate("09/11/2020")},
+            new object[] {TestUtils.GetDate("06/11/2020"), -1, TestUtils.GetDate("05/11/2020")},
+            new object[] {TestUtils.GetDate("09/11/2020"), 1, TestUtils.GetDate("10/11/2020")},
+            new object[] {TestUtils.GetDate("09/11/2020"), -1, TestUtils.GetDate("06/11/2020")},
+            new object[] {TestUtils.GetDate("06/11/2020"), 0, TestUtils.GetDate("06/11/2020")},
+
+        };
+        
+        private static object[] _dateCasesHoliday =
+        {
+            new object[] { 1, TestUtils.GetDate("08/11/2020"), TestUtils.GetDate("09/11/2020")},
+            new object[] { 1, TestUtils.GetDate("07/11/2020"), TestUtils.GetDate("09/11/2020")},
+            new object[] { 1, TestUtils.GetDate("06/11/2020"), TestUtils.GetDate("06/11/2020")},
+            new object[] {-1, TestUtils.GetDate("08/11/2020"), TestUtils.GetDate("06/11/2020")},
+            new object[] {-1, TestUtils.GetDate("07/11/2020"), TestUtils.GetDate("06/11/2020")},
+            new object[] {0, TestUtils.GetDate("06/11/2020"), TestUtils.GetDate("06/11/2020")},
+
+        };
+
+        [Test, TestCaseSource(nameof(_dateCasesBusiness))]
+        public void GetShiftBusinessDayTest(DateTime date, int shift, DateTime new_date)
+        {
+            Assert.AreEqual(new_date, _dayShiftService.GetShiftBusinessDay(date, shift));
+        }
+        
+        [Test, TestCaseSource(nameof(_dateCasesHoliday))]
+        public void ShiftWhileHolidayTest(int shift, DateTime date, DateTime newDate)
+        {
+            Assert.AreEqual(newDate, _dayShiftService.ShiftWhileHoliday(shift, date));
+        }
+    }
+}
